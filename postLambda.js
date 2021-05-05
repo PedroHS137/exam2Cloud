81d8d8010d1821217a6ab2d1d5e10e7b42c3df4d
Pedro Herrera <44147945+PedroHS137@users.noreply.github.com>
2021-05-04 23:30:10 -0500

    Add files via upload
    final version

--- postLambda.js
+++ postLambda.js
@@ -1,4 +1,34 @@
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const key = process.env.API_KEY;
+const url = process.env.API_URL;
+
+function httpPostIBM(theUrl,data) {
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", theUrl, false);
+    xhr.setRequestHeader('Authorization', 'Basic `{Username:apiKey}:${Password:Key}`'  );
+    //req.setRequestHeader('Authorization','Basic ' + Base64StringOfUserColonPassword);
+    //{"Username":"apiKey","Password":"Jm4ot5myQe8XbULR1rNI9YnGlF4ghho28tcTWJ5YgYU5"}
+	xhr.send(data);
+    return JSON.parse(xhr.responseText);
+}
+
 exports.handler = async (event) => {
-    
-    return event.historial_clinico;
+    let data= {
+        "features": {
+          "entities": {
+            "sentiment": true,
+            "emotion": true,
+            limit: 5
+          },
+          "keywords": {
+            "emotion": true,
+            "sentiment": true,
+            limit: 5
+          }
+        },          
+        "text": event.historial_clinico
+    }
+	let a=httpPostIBM(url,data);
+    return a;
+	
 };
